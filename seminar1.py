14ec92d27cdc05f1eea955f258fe4df013026fcc
Angelina Gridasova <gridasovaangelina@mail.ru>
2025-09-14 22:39:15 +0300

    lобавление цвета ко второму выводу

--- seminar1.py
+++ seminar1.py
@@ -1,10 +1,9 @@
 while True:
     answer = input("сделал лабу по элтеху? (Y/N)\n").strip().upper()
     if answer == 'Y':
-        print("\033[93mскинь пж\033[0m")
+        print("\033[93mскинь\033[0m")
         break
     elif answer == 'N':
         print("соболезную..")
-        break
     else:
         print("\033[91mY/N!!!\033[0m")
